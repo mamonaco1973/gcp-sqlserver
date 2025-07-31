e1f65dafec2e10e38fd61fa122aac86902fae4d5
mamonaco1973 <mamonaco1973@gmail.com>
2025-07-31 13:58:44 +0000

    fix validate.sh

--- validate.sh
+++ validate.sh
@@ -1,36 +1,35 @@
 # #!/bin/bash
 
-# #-------------------------------------------------------------------------------
-# # Output phpmyadmin URL and mysql DNS name
-# #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Output adminer URL and sqlserver DNS name
+#-------------------------------------------------------------------------------
 
-#  PHPMYADMIN_IP=$(gcloud compute instances describe phpmyadmin-vm \
-#    --zone=us-central1-a \
-#    --format='value(networkInterfaces[0].accessConfigs[0].natIP)')
+ADMINER_IP=$(gcloud compute instances describe adminer-vm \
+   --zone=us-central1-a \
+   --format='value(networkInterfaces[0].accessConfigs[0].natIP)')
 
-# echo "NOTE: phpMyAdmin running at http://$PHPMYADMIN_IP"
+echo "NOTE: Adminer running at http://$ADMINER_IP"
 
-# # Wait until the phpMyAdmin URL is reachable (HTTP 200 or similar)
-# echo "NOTE: Waiting for phpMyAdmin to become available at http://$PHPMYADMIN_IP ..."
+# Wait until the phpMyAdmin URL is reachable (HTTP 200 or similar)
+echo "NOTE: Waiting for Adminer to become available at http://$ADMINER_IP ..."
 
-# # Max attempts (optional)
-# MAX_ATTEMPTS=30
-# ATTEMPT=1
+# Max attempts (optional)
+MAX_ATTEMPTS=30
+ATTEMPT=1
 
-# until curl -s --head --fail "http://$PHPMYADMIN_IP" > /dev/null; do
-#   if [ "$ATTEMPT" -ge "$MAX_ATTEMPTS" ]; then
-#     echo "ERROR: phpMyAdmin did not become available after $MAX_ATTEMPTS attempts."
-#     exit 1
-#   fi
-#   echo "WARNING: phpMyAdmin not yet reachable. Retrying in 30 seconds..."
-#   sleep 30
-#   ATTEMPT=$((ATTEMPT+1))
-# done
+until curl -s --head --fail "http://$ADMINER_IP" > /dev/null; do
+   if [ "$ATTEMPT" -ge "$MAX_ATTEMPTS" ]; then
+     echo "ERROR: Adminer did not become available after $MAX_ATTEMPTS attempts."
+     exit 1
+   fi
+   echo "WARNING: Adminer not yet reachable. Retrying in 30 seconds..."
+   sleep 30
+   ATTEMPT=$((ATTEMPT+1))
+done
 
+SQLSERVER_DNS="sqlserver.internal.sqlserver-zone.local"
+echo "NOTE: Hostname for SQL Server is \"$SQLSERVER_DNS\""
 
-# MYSQL_DNS="mysql.internal.mysql-zone.local"
-# echo "NOTE: Hostname for mysql server is \"$MYSQL_DNS\""
-
-# #-------------------------------------------------------------------------------
-# # END OF SCRIPT
-# #-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# END OF SCRIPT
+#-------------------------------------------------------------------------------
